abe88df08d34f87f786fda276fcf106919e27034
ilya <ilya@gloryx.net>
2022-08-15 14:03:26 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.7.10"
+}
+
+group = "net.gloryx"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://dev.gloryx.net/main")
+}
+
+dependencies {
+}
